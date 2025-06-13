5de166ea33d00a7b26e858c72c6ed7c10eaeea6a
Le Hoang Than <lehoangthan584@gmail.com>
2025-06-14 00:11:43 +0700

    fix main.cpp

--- dsa-static-arrays-cpp/dsa-static-arrays-cpp/Main.cpp
+++ dsa-static-arrays-cpp/dsa-static-arrays-cpp/Main.cpp
@@ -1,4 +1,9 @@
-#include "include/Header.h"
+#include<iostream>
+#include "include/ArrayUtils.h"
+#include "include/Search.h"
+#include "include/Sort.h"
+#include "include/Menu.h"
+using namespace std; 
 int main()
 {
 	cout << "hello" << endl;
